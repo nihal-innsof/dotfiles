a79da053d6e05919e9c94160d704db35372da650
nihal <nihal@innsof.com>
2024-05-15 15:35:12 +0530

    updated .gitignore and added plugin in nvim to enable integration with tmux

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -378,6 +378,25 @@ local plugins = {
       debug = false -- Prints errors and the command which is run.
     }
   },
+
+  -- Tmux integration
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  }
 }
 
 require('lazy').setup(plugins, {})
