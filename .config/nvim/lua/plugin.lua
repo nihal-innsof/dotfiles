9482f8d6edc1ae1efe52485f49c433f2e30c6c61
nihal <nihal@innsof.com>
2024-05-20 17:50:54 +0530

    Installed latex plugin for nvim

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -354,16 +354,16 @@ local plugins = {
   },
 
   -- Custom Parameters (with defaults)
-  --[[ {
+  {
     "David-Kunz/gen.nvim",
     opts = {
       model = "zephyr",       -- The default model to use.
       host = "localhost",     -- The host running the Ollama service.
       port = "11434",         -- The port on which the Ollama service is listening.
-      display_mode = "float", -- The display mode. Can be "float" or "split".
+      display_mode = "split", -- The display mode. Can be "float" or "split".
       show_prompt = false,    -- Shows the Prompt submitted to Ollama.
-      show_model = false,     -- Displays which model you are using at the beginning of your chat session.
-      no_auto_close = false,  -- Never closes the window automatically.
+      show_model = true,      -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = true,   -- Never closes the window automatically.
       init = function(_) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
       -- Function to initialize Ollama
       command = function(options)
@@ -377,7 +377,7 @@ local plugins = {
       -- list_models = '<omitted lua function>', -- Retrieves a list of model names
       debug = false -- Prints errors and the command which is run.
     }
-  }, ]]
+  },
 
   -- Tmux integration
   {
@@ -396,6 +396,12 @@ local plugins = {
       { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
+  },
+  {
+    "lervag/vimtex",
+    lazy = false,     -- we don't want to lazy load VimTeX
+    init = function() -- VimTeX configuration goes here
+    end
   }
 }
 
